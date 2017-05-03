e7a782de7b00e4b76c1654965e69483a37025e01
Joseph Kato <joseph@jdkato.io>
2017-05-03 13:00:23 -0700

    refactor: better sentence case detection

--- check/variables.go
+++ check/variables.go
@@ -2,18 +2,24 @@ package check
 
 import (
 	"strings"
-	"unicode"
-	"unicode/utf8"
 
 	"github.com/jdkato/prose/transform"
 )
 
+// TODO: maybe use some string similarity metric for title case?
 func title(s string) bool { return s == transform.Title(s) }
 func lower(s string) bool { return s == strings.ToLower(s) }
 func upper(s string) bool { return s == strings.ToUpper(s) }
 func sentence(s string) bool {
-	first, n := utf8.DecodeRuneInString(s)
-	return s == string(unicode.ToTitle(first))+strings.ToLower(s[n:])
+	count := 0.0
+	words := 0.0
+	for i, w := range strings.Fields(s) {
+		if i > 0 && w == strings.Title(w) {
+			count++
+		}
+		words++
+	}
+	return (count / words) < 0.4
 }
 
 var varToFunc = map[string]func(string) bool{
