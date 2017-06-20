errata-ai/vale

commit d8ce6cb1ca6753d035ee716d09a817111b7d1496

fix: respect all CL arguments (not just the first)
Fixes #46

main.go
@@ -96,23 +96,16 @@ func main() {
 		var linted []*core.File
 		var err error
 		var hasAlerts bool
-		var src string
 
 		if c.NArg() > 0 || core.Stat() {
-			mgr := check.NewManager(config)
+			linter := lint.Linter{
+				Config: config, CheckManager: check.NewManager(config)}
+
 			if c.NArg() > 0 {
-				src = c.Args()[0]
+				linted, err = linter.Lint(c.Args(), glob)
 			} else {
 				stdin, _ := ioutil.ReadAll(os.Stdin)
-				src = string(stdin)
-			}
-
-			linter := lint.Linter{Config: config, CheckManager: mgr}
-			// Do we a directory/file or a string?
-			if core.IsDir(src) || core.FileExists(src) {
-				linted, err = linter.Lint(src, glob)
-			} else {
-				linted, err = linter.LintString(src)
+				linted, err = linter.LintString(string(stdin))
 			}
 
 			// How should we style the output?

features/CLI.feature
@@ -1,0 +1,69 @@
+Feature: CLI
+  Scenario: Lint a file and a directory
+    When I lint "test.json subdir1"
+    Then the output should contain exactly:
+    """
+    subdir1/test.hs:2:4:vale.Annotations:'NOTE' left in text
+    subdir1/test.hs:5:6:vale.Annotations:'TODO' left in text
+    subdir1/test.hs:6:25:vale.Annotations:'XXX' left in text
+    subdir1/test.hs:11:41:vale.Annotations:'XXX' left in text
+    subdir1/test.rs:1:5:vale.Annotations:'NOTE' left in text
+    subdir1/test.rs:3:5:vale.Annotations:'XXX' left in text
+    subdir1/test.rs:5:17:vale.Annotations:'TODO' left in text
+    subdir1/test.rs:7:4:vale.Annotations:'FIXME' left in text
+    subdir1/test.rs:9:5:vale.Annotations:'XXX' left in text
+    test.json:9:10:vale.Annotations:'XXX' left in text
+    test.json:12:19:vale.Annotations:'TODO' left in text
+    """
+    And the exit status should be 0
+
+  Scenario: Lint 2 directories
+    When I lint "subdir1 subdir2"
+    Then the output should contain exactly:
+    """
+    subdir1/test.hs:2:4:vale.Annotations:'NOTE' left in text
+    subdir1/test.hs:5:6:vale.Annotations:'TODO' left in text
+    subdir1/test.hs:6:25:vale.Annotations:'XXX' left in text
+    subdir1/test.hs:11:41:vale.Annotations:'XXX' left in text
+    subdir1/test.rs:1:5:vale.Annotations:'NOTE' left in text
+    subdir1/test.rs:3:5:vale.Annotations:'XXX' left in text
+    subdir1/test.rs:5:17:vale.Annotations:'TODO' left in text
+    subdir1/test.rs:7:4:vale.Annotations:'FIXME' left in text
+    subdir1/test.rs:9:5:vale.Annotations:'XXX' left in text
+    subdir2/test.lua:1:4:vale.Annotations:'NOTE' left in text
+    subdir2/test.lua:2:19:vale.Annotations:'XXX' left in text
+    subdir2/test.lua:5:7:vale.Annotations:'NOTE' left in text
+    subdir2/test.lua:9:6:vale.Annotations:'XXX' left in text
+    subdir2/test.lua:15:4:vale.Annotations:'TODO' left in text
+    """
+    And the exit status should be 0
+
+  Scenario: Lint 2 files
+    When I lint "test.json test.txt"
+    Then the output should contain exactly:
+    """
+    test.json:9:10:vale.Annotations:'XXX' left in text
+    test.json:12:19:vale.Annotations:'TODO' left in text
+    test.txt:1:27:vale.Annotations:'NOTE' left in text
+    test.txt:4:12:vale.Annotations:'XXX' left in text
+    test.txt:4:66:vale.Annotations:'TODO' left in text
+    """
+    And the exit status should be 0
+
+  Scenario: Lint 3 files
+    When I lint "test.json test.txt test.cc"
+    Then the output should contain exactly:
+    """
+    test.cc:1:4:vale.Annotations:'XXX' left in text
+    test.cc:9:6:vale.Annotations:'NOTE' left in text
+    test.cc:13:6:vale.Annotations:'XXX' left in text
+    test.cc:17:5:vale.Annotations:'FIXME' left in text
+    test.cc:20:5:vale.Annotations:'XXX' left in text
+    test.cc:23:37:vale.Annotations:'XXX' left in text
+    test.json:9:10:vale.Annotations:'XXX' left in text
+    test.json:12:19:vale.Annotations:'TODO' left in text
+    test.txt:1:27:vale.Annotations:'NOTE' left in text
+    test.txt:4:12:vale.Annotations:'XXX' left in text
+    test.txt:4:66:vale.Annotations:'TODO' left in text
+    """
+    And the exit status should be 0

fixtures/formats/subdir1/test.hs
@@ -1,0 +1,14 @@
+square :: Int -> Int
+-- NOTE: ^The 'square' function squares an integer.
+square x = x * x
+
+-- | TODO: The 'square' function squares an integer.
+square :: Int -> Int -- XXX: this is an Int
+square x = x * x
+
+{-|
+  The 'square' function squares an integer.
+  It takes one argument, of type 'Int'. XXX:
+-}
+square :: Int -> Int
+square x = x * x

fixtures/formats/subdir1/test.rs
@@ -1,0 +1,14 @@
+//! NOTE: this is a doc comment.
+//!
+//! XXX: still a doc comment.
+
+let XXX = 5; // TODO: This is a line comment.
+
+// FIXME: here's another line comment
+
+/// XXX: another doc comment
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+let TODO = 0;

fixtures/formats/subdir2/test.lua
@@ -1,0 +1,15 @@
+-- NOTE: this is a comment
+print ("hello" -- XXX: multiple comment -- yeah!
+        )
+
+---[[ NOTE: this is a comment with code?!
+print ("iuwiuqweiouqwe")
+--]]
+
+--[[ XXX: This is a comment
+and I am going on and on
+and on ]] print ("17")
+
+print ("17")
+
+-- TODO: 'This is a comment'

lint/lint.go
@@ -69,18 +69,23 @@ func (l Linter) LintString(src string) ([]*core.File, error) {
 }
 
 // Lint src according to its format.
-func (l Linter) Lint(src string, pat string) ([]*core.File, error) {
+func (l Linter) Lint(input []string, pat string) ([]*core.File, error) {
 	var linted []*core.File
 
 	done := make(chan core.File)
 	defer close(done)
 
-	filesChan, errc := l.lintFiles(done, src, core.NewGlob(pat))
-	for f := range filesChan {
-		linted = append(linted, f)
-	}
-	if err := <-errc; err != nil {
-		return nil, err
+	for _, src := range input {
+		if !(core.IsDir(src) || core.FileExists(src)) {
+			continue
+		}
+		filesChan, errc := l.lintFiles(done, src, core.NewGlob(pat))
+		for f := range filesChan {
+			linted = append(linted, f)
+		}
+		if err := <-errc; err != nil {
+			return nil, err
+		}
 	}
 
 	if l.Config.Sorted {

lint/lint_test.go
@@ -55,7 +55,7 @@ func benchmarkLint(path string, b *testing.B) {
 	mgr := check.NewManager(config)
 	linter := Linter{Config: config, CheckManager: mgr}
 	for n := 0; n < b.N; n++ {
-		_, _ = linter.Lint(path, "*")
+		_, _ = linter.Lint([]string{path}, "*")
 	}
 }
 

commit 12e28ab37c0c4d9f2d65cbf87c2f0ffd88538519

fix: correctly recognize non-piped std input
This was broken in d8ce6cb1ca6753d035ee716d09a817111b7d1496.

core/util.go
@@ -275,6 +275,11 @@ func CheckError(err error) bool {
 	return err == nil
 }
 
+// LooksLikeStdin determines if s appears to be a string.
+func LooksLikeStdin(s string) bool {
+	return !(FileExists(s) || IsDir(s))
+}
+
 // CheckAndClose closes `file` and prints any errors to stdout.
 // A return value of true => no error.
 func CheckAndClose(file *os.File) bool {

main.go
@@ -107,7 +107,11 @@ func main() {
 				Config: config, CheckManager: check.NewManager(config)}
 
 			if c.NArg() > 0 {
-				linted, err = linter.Lint(c.Args(), glob)
+				if core.LooksLikeStdin(c.Args()[0]) {
+					linted, err = linter.LintString(c.Args()[0])
+				} else {
+					linted, err = linter.Lint(c.Args(), glob)
+				}
 			} else {
 				stdin, _ := ioutil.ReadAll(os.Stdin)
 				linted, err = linter.LintString(string(stdin))