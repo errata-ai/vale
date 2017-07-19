dcb21f72e1a6644b96395a1ea09b3a78c54483f1
Joseph Kato <joseph@jdkato.io>
2017-07-19 10:49:38 -0700

    chore: update .gitignore

--- _vendor-20170719104338/github.com/jdkato/prose/scripts/pipe.py
+++
@@ -1,8 +0,0 @@
-from nltk.corpus import treebank
-from nltk.tag.util import untag
-
-sentences = treebank.tagged_sents()
-text = []
-for s in sentences:
-    text.append(' '.join(untag(s)))
-print(' '.join(text))
--- _vendor-20170719104338/github.com/jdkato/prose/transform/transform_test.go
+++
@@ -1,9 +0,0 @@
-package transform
-
-import "fmt"
-
-func ExampleNewTitleConverter() {
-	tc := NewTitleConverter(APStyle)
-	fmt.Println(tc.Title("the last of the mohicans"))
-	// Output: The Last of the Mohicans
-}
