b6c3f9d45c1b942d302136547ae9bfdcd82c2951
Joseph Kato <joseph@jdkato.io>
2018-04-30 15:10:17 -0700

    refactor: add YAML documentation to definitions

--- check/defintions.go
+++ check/defintions.go
@@ -29,79 +29,109 @@ type Definition struct {
 // Existence checks for the present of Tokens.
 type Existence struct {
 	Definition `mapstructure:",squash"`
-	Append     bool
+	// `append` (`bool`): Adds `raw` to the end of `tokens`, assuming both are defined.
+	Append bool
+	// `ignorecase` (`bool`): Makes all matches case-insensitive.
 	Ignorecase bool
-	Nonword    bool
-	Raw        []string
-	Tokens     []string
+	// `nonword` (`bool`): Removes the default word boundaries (`\b`).
+	Nonword bool
+	// `raw` (`array`): A list of tokens to be concatenated into a pattern.
+	Raw []string
+	// `tokens` (`array`): A list of tokens to be transformed into a non-capturing group.
+	Tokens []string
 }
 
 // Substitution switches the values of Swap for its keys.
 type Substitution struct {
 	Definition `mapstructure:",squash"`
+	// `ignorecase` (`bool`): Makes all matches case-insensitive.
 	Ignorecase bool
-	Nonword    bool
-	Swap       map[string]string
-	POS        string
+	// `nonword` (`bool`): Removes the default word boundaries (`\b`).
+	Nonword bool
+	// `swap` (`map`): A sequence of `observed: expected` pairs.
+	Swap map[string]string
+	// `pos` (`string`): A regular expression matching tokens to parts of speech.
+	POS string
 }
 
 // Occurrence counts the number of times Token appears.
 type Occurrence struct {
 	Definition `mapstructure:",squash"`
-	Max        int
-	Token      string
+	// `max` (`int`): The maximum amount of times `token` may appear in a given scope.
+	Max int
+	// `token` (`string`): The token of interest.
+	Token string
 }
 
 // Repetition looks for repeated uses of Tokens.
 type Repetition struct {
 	Definition `mapstructure:",squash"`
 	Max        int
+	// `ignorecase` (`bool`): Makes all matches case-insensitive.
 	Ignorecase bool
-	Alpha      bool
-	Tokens     []string
+	// `alpha` (`bool`): Limits all matches to alphanumeric tokens.
+	Alpha bool
+	// `tokens` (`array`): A list of tokens to be transformed into a non-capturing group.
+	Tokens []string
 }
 
 // Consistency ensures that the keys and values of Either don't both exist.
 type Consistency struct {
 	Definition `mapstructure:",squash"`
-	Nonword    bool
+	// `nonword` (`bool`): Removes the default word boundaries (`\b`).
+	Nonword bool
+	// `ignorecase` (`bool`): Makes all matches case-insensitive.
 	Ignorecase bool
-	Either     map[string]string
+	// `either` (`map`): A map of `option 1: option 2` pairs, of which only one may appear.
+	Either map[string]string
 }
 
 // Conditional ensures that the present of First ensures the present of Second.
 type Conditional struct {
 	Definition `mapstructure:",squash"`
+	// `ignorecase` (`bool`): Makes all matches case-insensitive.
 	Ignorecase bool
-	First      string
-	Second     string
+	// `first` (`string`): The antecedent of the statement.
+	First string
+	// `second` (`string`): The consequent of the statement.
+	Second string
+	// `exceptions` (`array`): An array of strings to be ignored.
 	Exceptions []string
 }
 
 // Capitalization checks the case of a string.
 type Capitalization struct {
 	Definition `mapstructure:",squash"`
-	Match      string
-	Check      func(string) bool
-	Style      string
+	// `match` (`string`): $title, $sentence, $lower, $upper, or a pattern.
+	Match string
+	Check func(string) bool
+	// `style` (`string`): AP or Chicago; only applies when match is set to $title.
+	Style string
 }
 
 // Readability checks the reading grade level of text.
 type Readability struct {
 	Definition `mapstructure:",squash"`
-	Metrics    []string
-	Grade      float64
+	// `metrics` (`array`): One or more of Gunning Fog, Coleman-Liau, Flesch-Kincaid, SMOG, and Automated Readability.
+	Metrics []string
+	// `grade` (`float`): The highest acceptable score.
+	Grade float64
 }
 
 // Spelling checks text against a Hunspell dictionary.
 type Spelling struct {
 	Definition `mapstructure:",squash"`
-	Aff        string
-	Custom     bool
-	Dic        string
-	Filters    []*regexp.Regexp
-	Ignore     string
-	Threshold  int
+	// `aff` (`string`): The fully-qualified path to a Hunspell-compatible `.aff` file.
+	Aff string
+	// `custom` (`bool`): Turn off the default filters for acronyms, abbreviations, and numbers.
+	Custom bool
+	// `dic` (`string`): The fully-qualified path to a Hunspell-compatible `.dic` file.
+	Dic string
+	// `filters` (`array`): An array of patterns to ignore during spell checking.
+	Filters []*regexp.Regexp
+	// `ignore` (`string`): A relative path to a personal vocabulary file.
+	Ignore    string
+	Threshold int
 }
 
 var defaultRules = []string{
