de041c6e64553e03b9a557d159065a1bcbbcc3d5
Joseph Kato <joseph@jdkato.io>
2018-08-08 15:22:27 -0700

    refactor: remove scripts

--- scripts/compress.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-
-upx dist/vale_Windows_64-bit/vale.exe
-upx dist/vale_Linux_64-bit/vale
--- scripts/sign.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd $1
-
-set -x
-rm -f *.asc
-for f in *; do
-    gpg --detach-sign --armor "$f"
-done
