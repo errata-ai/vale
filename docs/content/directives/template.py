1bcb421c744565e883bfa0a8142bd70cc7abdf3e
Joseph Kato <joseph@jdkato.io>
2019-12-03 11:50:41 -0800

    docs: update directives

--- docs/content/directives/template.py
+++ docs/content/directives/template.py
@@ -7,5 +7,5 @@ vale = os.path.abspath(os.path.join("..", "docs", "bin", "vale"))
 def main(extends):
     """return an example for the given Vale extension point.
     """
-    example = subprocess.check_output([vale, "new", extends])
+    example = subprocess.check_output([vale, "new-rule", extends])
     return example.decode("utf-8").strip()
