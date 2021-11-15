2477819be6f0922093fd197b71d685666ecc5d1c
Joseph Kato <joseph@jdkato.io>
2021-11-15 00:04:45 -0800

    refactor: more formatting improvements

--- internal/cli/flag.go
+++ internal/cli/flag.go
@@ -1,7 +1,10 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/errata-ai/vale/v2/internal/core"
+	"github.com/pterm/pterm"
 	"github.com/spf13/pflag"
 )
 
@@ -23,17 +26,26 @@ var shortcodes = map[string]string{
 }
 
 func init() {
-	pflag.StringVarP(&Flags.Sources, "sources", "s", "", "config files to load")
-	pflag.StringVarP(&Flags.Glob, "glob", "g", "*", `A glob pattern (e.g., --glob='*.{md,txt}).'`)
-	pflag.StringVarP(&Flags.Path, "config", "c", "", `A file path (e.g., --config='some/file/path/.vale.ini').`)
-	pflag.StringVarP(&Flags.AlertLevel, "minAlertLevel", "m", "", `Lowest alert level to display (e.g., --minAlertLevel=error).`)
-	pflag.StringVarP(&Flags.Output, "output", "o", "CLI", `Output style ("line", "JSON", or a template file).`)
-	pflag.StringVarP(&Flags.InExt, "ext", "e", ".txt", `Extension to associate with stdin (e.g., --ext=.md).`)
+	pflag.StringVarP(&Flags.Sources, "sources", "s", "", "A config files to load")
+
+	pflag.StringVarP(&Flags.Glob, "glob", "g", "*",
+		fmt.Sprintf(`A glob pattern (e.g., %s)`, pterm.Gray(`--glob='*.{md,txt}.'`)))
+
+	pflag.StringVarP(&Flags.Path, "config", "c", "",
+		fmt.Sprintf(`A file path (e.g., %s).`, pterm.Gray(`--config='some/file/path/.vale.ini'`)))
+
+	pflag.StringVarP(&Flags.AlertLevel, "minAlertLevel", "m", "",
+		fmt.Sprintf(`The lowest alert level to display (e.g., %s).`, pterm.Gray(`--minAlertLevel=error`)))
+
+	pflag.StringVarP(&Flags.Output, "output", "o", "CLI", `An output style ("line", "JSON", or a template file).`)
+
+	pflag.StringVarP(&Flags.InExt, "ext", "e", ".txt",
+		fmt.Sprintf(`An extension to associate with stdin (e.g., %s).`, pterm.Gray(`--ext=.md`)))
 
 	pflag.BoolVarP(&Flags.Wrap, "no-wrap", "w", false, "Don't wrap CLI output.")
 	pflag.BoolVarP(&Flags.NoExit, "no-exit", "x", false, "Don't return a nonzero exit code on errors.")
 	pflag.BoolVarP(&Flags.Simple, "ignore-syntax", "n", false, "Lint all files line-by-line.")
-	pflag.BoolVarP(&Flags.Version, "version", "v", false, "Print current version.")
+	pflag.BoolVarP(&Flags.Version, "version", "v", false, "Print the current version.")
 	pflag.BoolVarP(&Flags.Help, "help", "h", false, "Print this help message.")
 
 	pflag.BoolVar(&Flags.Local, "mode-compat", false, "prioritize local Vale configurations")
