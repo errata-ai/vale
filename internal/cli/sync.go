9503da0ebdb6bdb4ff0838c95f6b801c60d83d4b
jdkato <joseph@jdkato.io>
2022-05-10 10:22:00 -0700

    fix: don't use `os.rename`

--- internal/cli/sync.go
+++ internal/cli/sync.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -155,7 +156,7 @@ func moveAsset(name, old, new string) error {
 	}
 
 	os.MkdirAll(new, 0755)
-	if err := os.Rename(src, dst); err != nil {
+	if err := moveFile(src, dst); err != nil {
 		return err
 	}
 
@@ -224,4 +225,28 @@ func getLibrary(path string) ([]Style, error) {
 	})
 
 	return styles, err
+}
+
+func moveFile(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("couldn't open source file: %s", err)
+	}
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		inputFile.Close()
+		return fmt.Errorf("couldn't open dest file: %s", err)
+	}
+	defer outputFile.Close()
+	_, err = io.Copy(outputFile, inputFile)
+	inputFile.Close()
+	if err != nil {
+		return fmt.Errorf("writing to output file failed: %s", err)
+	}
+	// The copy was successful, so now delete the original file
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("failed removing original file: %s", err)
+	}
+	return nil
 }
