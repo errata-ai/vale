aa8c95e9b196f2b3b9dd34e39466f485348e3e9b
Michael Lombardi <mlombardi@microsoft.com>
2023-03-07 14:43:59 -0600

    (MAINT) Add tests for PowerShell code linting

---
+++ testdata/fixtures/formats/test.ps1
@@ -0,0 +1,9 @@
+function Test-Vale {
+    "This is a test" # XXX: this is a line comment.
+    <#
+        NOTE: this is a multiline comment
+        FIXME: this is too!
+    #>
+    "TODO is yet another test"
+    "FIXME Final Test" # TODO: Here's a comment
+}
