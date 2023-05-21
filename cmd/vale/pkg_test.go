1040f07a3e83d1c22194cab02663188934fbb65e
jdkato <joseph@jdkato.io>
2023-05-21 02:58:34 -0700

    test: add more Package cases

--- cmd/vale/pkg_test.go
+++ cmd/vale/pkg_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/errata-ai/vale/v2/internal/core"
@@ -93,4 +95,78 @@ func TestLocalDir(t *testing.T) {
 	if !core.FileExists(filepath.Join(path, "write-good", "E-Prime.yml")) {
 		t.Fatal("unable to find 'E-Prime' in StylesPath")
 	}
+}
+
+func TestLocalComplete(t *testing.T) {
+	path, err := mockPath()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	zip, err := filepath.Abs(filepath.Join(TEST_DATA, "ISC.zip"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = readPkg(zip, path, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !core.IsDir(filepath.Join(path, "ISC")) {
+		t.Fatal("unable to find 'ISC' in StylesPath")
+	}
+
+	vocab := filepath.Join(path, "Vocab", "ISC_General", "accept.txt")
+	if !core.FileExists(vocab) {
+		t.Fatal("unable to find 'ISC_General' in Vocab")
+	}
+
+	b, err := os.ReadFile(vocab)
+	if err != nil {
+		t.Fatal(err)
+	}
+	lines := strings.Split(string(b), "\n")
+
+	if !core.StringInSlice("bar", lines) {
+		t.Fatal(fmt.Sprintf("unable to find 'bar' in %v", lines))
+	}
+
+}
+
+func TestLocalOnlyStyles(t *testing.T) {
+	path, err := mockPath()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	zip, err := filepath.Abs(filepath.Join(TEST_DATA, "OnlyStyles.zip"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = readPkg(zip, path, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !core.IsDir(filepath.Join(path, "ISC")) {
+		t.Fatal("unable to find 'ISC' in StylesPath")
+	}
+
+	vocab := filepath.Join(path, "Vocab", "ISC_General", "accept.txt")
+	if !core.FileExists(vocab) {
+		t.Fatal("unable to find 'ISC_General' in Vocab")
+	}
+
+	b, err := os.ReadFile(vocab)
+	if err != nil {
+		t.Fatal(err)
+	}
+	lines := strings.Split(string(b), "\n")
+
+	if !core.StringInSlice("bar", lines) {
+		t.Fatal(fmt.Sprintf("unable to find 'bar' in %v", lines))
+	}
+
 }
